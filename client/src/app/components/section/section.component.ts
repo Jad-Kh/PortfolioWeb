82a432e13bd76502485a52abf5efc3fe1355255e
Situu <70975136+Jad-Kh@users.noreply.github.com>
2022-03-04 18:16:35 +0200

    display dynamic portfolio

--- client/src/app/components/section/section.component.ts
+++ client/src/app/components/section/section.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from "rxjs";
+import { map } from 'rxjs/operators';
+
+import { SectionService } from "src/app/services/section-service";
+import { AuthService } from "src/app/services/auth-service";
+
+import { Section } from "src/app/models/Section";
+import { User } from "src/app/models/User";
+
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
@@ -7,9 +16,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionComponent implements OnInit {
 
-  constructor() { }
+  sections$!: Observable<Section[]>;
+  userId!: Pick<User, "id">;
+
+  constructor(
+    private sectionService: SectionService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.sections$ = this.fetchForUser();
+    this.userId = this.authService.userId;
+  }
+
+  fetchForUser(): Observable<Section[]> {
+    return this.sectionService.fetchForUser()
+      .pipe (
+        map(sections => 
+            sections.filter(section => section.title.toLowerCase() !== "about"
+                                    && section.title.toLowerCase() !== "languages"
+                                    && section.title.toLowerCase() !== "personal"
+           ))
+      );
   }
 
 }
